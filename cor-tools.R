0047b95233617c3d517f6ecae46987a702c974fb
Arni Magnusson <thisisarni@gmail.com>
2022-08-06 11:13:07 +1100

    Source corLabel.R

--- cor-tools.R
+++ cor-tools.R
@@ -1,3 +1,3 @@
 source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/corHist.R")
-source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/corLabels.R")
+source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/corLabel.R")
 source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/five.R")
