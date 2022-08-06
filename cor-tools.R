6a0a2ddbdc8eb7e827f67860bb42a6945308271a
Arni Magnusson <thisisarni@gmail.com>
2022-08-06 11:08:46 +1100

    Installation script

---
+++ cor-tools.R
@@ -0,0 +1,3 @@
+source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/corHist.R")
+source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/corLabels.R")
+source("https://raw.githubusercontent.com/PacificCommunity/ofp-sam-r-sandbox/main/cor-tools/five.R")
