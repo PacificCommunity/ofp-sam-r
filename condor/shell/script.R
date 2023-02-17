0b98a963b813070a7a68319d17051c1b39a541cf
Arni Magnusson <thisisarni@gmail.com>
2023-02-17 16:59:04 +1100

    Read data.txt

--- condor/shell/script.R
+++ condor/shell/script.R
@@ -1,5 +1,7 @@
 #!/usr/bin/Rscript
 
-fm <- lm(dist~speed, cars)
+data <- read.table("data.txt", header=TRUE)
+
+fm <- lm(dist~speed, data)
 results <- trimws(capture.output(summary(fm)))
 writeLines(results, "results.txt")
