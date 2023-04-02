a8a9f2f21db9b0d357e2002b8318be4511901639
Arni Magnusson <thisisarni@gmail.com>
2023-04-03 09:18:11 +1100

    Exclude _condor_*, tmp, var

--- condor/03_start_package/condor_run.sh
+++ condor/03_start_package/condor_run.sh
@@ -2,4 +2,5 @@
 
 tar -xzf Start.tar.gz
 Rscript script.R
-tar -czf End.tar.gz --exclude '*.tar.gz' *
+tar -czf End.tar.gz --exclude '*.tar.gz' --exclude '_condor_*'\
+    --exclude 'tmp' --exclude 'var' *
